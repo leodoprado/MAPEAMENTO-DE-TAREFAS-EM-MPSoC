3682d89fb16e2eeec8d44d2d53ffaabf51b13e14
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-09-05 15:50:52 -0300

    Atualizando testes

--- MPSoC-Map-YX.js
+++ MPSoC-Map-YX.js
@@ -1,4 +1,4 @@
-var data = require('./AppsDesafio.json')
+var data = require('./AppsDesafioMap.json')
 
 var tarefas = data.aplicacoes;
 
@@ -10,13 +10,41 @@ for(var i = 0; i < linhas; i++){
     matriz[i] = new Array(colunas).fill(0);
 }
 
-exibe(matriz)
-
 function exibe(matriz){
     for(let i = 0; i < linhas; i++){
         console.log(matriz[i])
     }
 }
 
-// Mapeamento do MPSoC
+exibe(matriz)
 
+let contMain = 0;
+
+// Mapeamento do MPSoC
+while(tarefas.length > contMain){
+    let map = tarefas[contMain].map
+    let contMap = 0;
+
+    while(map.length > contMap){
+        let mapIndex = map
+        let contIndex = map.length
+        //console.log(contIndex)
+        let contLinhas;
+        let contColunas = 0;
+        //console.log(matriz[1][1] = mapIndex[0])
+        for(contLinhas = 0; contLinhas < linhas; contLinhas++){
+            if(contLinhas >= linhas){
+                contLinhas = 0;
+                contColunas++;
+                matriz[contLinhas][contColunas] = mapIndex[contIndex]
+            } else {
+                matriz[contLinhas][contColunas] = mapIndex[contIndex]
+                contLinhas++;
+            }
+        } 
+        exibe(matriz)
+        console.log('-------------')
+        contMap++;
+    }
+    contMain++;
+}

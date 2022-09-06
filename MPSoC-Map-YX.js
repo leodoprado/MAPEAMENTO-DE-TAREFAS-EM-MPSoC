0e292dcbc1d567f6f46cdf4ddd8756385c11309a
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-09-06 10:37:50 -0300

    Algoritmo de Mapeamento

--- MPSoC-Map-YX.js
+++ MPSoC-Map-YX.js
@@ -2,49 +2,42 @@ var data = require('./AppsDesafioMap.json')
 
 var tarefas = data.aplicacoes;
 
-var linhas = 3
-var colunas = 4
-
-var matriz = new Array(linhas)
-for(var i = 0; i < linhas; i++){
-    matriz[i] = new Array(colunas).fill(0);
-}
-
 function exibe(matriz){
+    console.log('---------------')
     for(let i = 0; i < linhas; i++){
         console.log(matriz[i])
     }
+    console.log('---------------')
 }
 
-exibe(matriz)
-
-let contMain = 0;
-
 // Mapeamento do MPSoC
+let contMain = 0;
 while(tarefas.length > contMain){
+
+    var linhas = tarefas[contMain].linhas
+    var colunas = tarefas[contMain].colunas
+
+    var matriz = new Array(linhas)
+    for(var i = 0; i < linhas; i++){
+        matriz[i] = new Array(colunas).fill(0);
+    }
+
     let map = tarefas[contMain].map
     let contMap = 0;
+    let indexMapLinha = 0
+    let indexMapColuna = 0
 
     while(map.length > contMap){
-        let mapIndex = map
-        let contIndex = map.length
-        //console.log(contIndex)
-        let contLinhas;
-        let contColunas = 0;
-        //console.log(matriz[1][1] = mapIndex[0])
-        for(contLinhas = 0; contLinhas < linhas; contLinhas++){
-            if(contLinhas >= linhas){
-                contLinhas = 0;
-                contColunas++;
-                matriz[contLinhas][contColunas] = mapIndex[contIndex]
-            } else {
-                matriz[contLinhas][contColunas] = mapIndex[contIndex]
-                contLinhas++;
-            }
-        } 
-        exibe(matriz)
-        console.log('-------------')
+        if(indexMapLinha !== linhas){
+            matriz[indexMapLinha][indexMapColuna] = map[contMap]
+            indexMapLinha++;
+        } else {
+            indexMapLinha = 0
+            indexMapColuna++;
+            matriz[indexMapLinha][indexMapColuna] = map[contMap]
+        }
         contMap++;
     }
+    exibe(matriz)
     contMain++;
 }
---
+++ logica-do-algoritmo.md
@@ -0,0 +1,9 @@
+# Algoritmo de mapeamento Y -> X
+
+## 1 - Percorrer n casos de mapeamento
+    * A cada interação do laço principal
+    * Com mapa de calor (n -> packages)
+
+## 2 - Percorrer os indíces e atribuir a matriz (linha -> coluna)
+    * A cada interação do laço principal, deverá ser atribuido n vezes, independente do tamanho da matriz
+    *

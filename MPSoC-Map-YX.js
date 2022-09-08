9cc2df411e696e7494f8654a3240bb9b2ae40a7a
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-09-08 01:46:04 -0300

    Alterando algoritmo de mapeamento

--- MPSoC-Map-YX.js
+++ MPSoC-Map-YX.js
@@ -2,9 +2,9 @@ var data = require('./AppsDesafioMap.json')
 
 var tarefas = data.aplicacoes;
 
-function exibe(matriz){
+function exibe(matriz) {
     console.log('Y')
-    for(let i = 0; i < linhas; i++){
+    for (let i = 0; i < linhas; i++) {
         console.log("| " + matriz[i])
     }
     console.log('--------------> X\n')
@@ -12,13 +12,13 @@ function exibe(matriz){
 
 // Mapeamento do MPSoC 
 let contMain = 0;
-while(tarefas.length > contMain){
+while (tarefas.length > contMain) {
 
     var linhas = tarefas[contMain].linhas
     var colunas = tarefas[contMain].colunas
 
     var matriz = new Array(linhas)
-    for(var i = 0; i < linhas; i++){
+    for (var i = 0; i < linhas; i++) {
         matriz[i] = new Array(colunas).fill(0);
     }
 
@@ -27,14 +27,29 @@ while(tarefas.length > contMain){
     let indexMapLinha = 0
     let indexMapColuna = 0
 
-    while(map.length > contMap){
-        if(indexMapLinha !== linhas){
+    let contLinhas = linhas
+    let contColunas = colunas
+    let auxLinha = 0;
+    let auxColuna = 0;
+
+    while (map.length > contMap) {
+        if (indexMapLinha < contLinhas) {
             matriz[indexMapLinha][indexMapColuna] = map[contMap]
             indexMapLinha++;
-        } else {
-            indexMapLinha = 0
-            indexMapColuna++;
-            matriz[indexMapLinha][indexMapColuna] = map[contMap]
+
+            if(indexMapLinha === contLinhas) {
+                auxColuna++;
+                indexMapColuna = auxColuna;
+            }
+        } else if (indexMapColuna < contColunas) {
+            indexMapColuna++
+            matriz[indexMapLinha-1][indexMapColuna-1] = map[contMap]
+            
+            if(indexMapColuna === contColunas) {
+                indexMapLinha = 0;
+                indexMapColuna = auxColuna;
+                contLinhas--;
+            }
         }
         contMap++;
     }

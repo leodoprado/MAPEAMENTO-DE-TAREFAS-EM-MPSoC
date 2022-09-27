dcd6999ad2f1e933189c4dee1fb61348209f93c4
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-09-27 10:53:39 -0300

    Att 17-09

--- MPSoC-YX.js
+++ MPSoC-YX.js
@@ -8,26 +8,72 @@ console.log(testData)
 
 let contMain = 0;
 
-// Acessando cada posição do testData.TEST -> map
+// Acessando posições das tasks para mapear
 testData.TEST.forEach(TEST => {
-    let arrayMap = []
-    // acessando parametros para entrar no loop de TEST e Applications
-    console.log("\n")
+    console.log("\n----------------------------------------------------------------------------------\n")
+    
+    let arrMap = []
+
     console.log(TEST.APP) 
     console.log(TEST.QTD) 
+    console.log("\n")
     
     let Application = require(`./Teste-TrabalhoMapeamento/TrabalhoMapeamento/Applications/${TEST.APP}`)
     console.log(Application)
     console.log("\n")
 
-    // Buscando o grafo de tarefas para mapear
-    Application.grafo_tarefas.forEach(GrafoTarefas => {
-        arrayMap.push(GrafoTarefas.tarefa_origem, GrafoTarefas.tarefa_destino) 
-        var unique = [...new Set(arrayMap)];
-        console.log(unique);
-    })
+    // Filtrando o array de tarefas para mapear
+    let contTarefas = 0
+    while(Application.grafo_tarefas.length > contTarefas){
 
+        arrMap.push(Application.grafo_tarefas[contTarefas].tarefa_origem, Application.grafo_tarefas[contTarefas].tarefa_destino) 
+        var arrayMap = [...new Set(arrMap)];
+        contTarefas++;
+    }
 
+    console.log(arrayMap)
+
+    //////////////////////////
+
+    let YLinhas = testData.MPSOC_SIZE_Y
+    let XColunas = testData.MPSOC_SIZE_X
+
+    var matriz = new Array(YLinhas)
+    for (var i = 0; i < YLinhas; i++) {
+        matriz[i] = new Array(8).fill(0);
+    }
+
+    // Algoritmo de mapeamento
+    let indexMapLinha = 0
+    let indexMapColuna = 0
+    let contLinhas = YLinhas
+    let contColunas = XColunas
+    let auxLinha = 0;
+    let auxColuna = 0;
+    let contMap = 0;
+
+    while (arrayMap.length > contMap){
+        if (indexMapLinha < contLinhas) {
+            matriz[indexMapLinha][indexMapColuna] = arrayMap[contMap]
+            indexMapLinha++;
+
+            if(indexMapLinha === contLinhas) {
+                auxColuna++;
+                indexMapColuna = auxColuna;
+            }
+        } else if (indexMapColuna < contColunas) {
+            indexMapColuna++
+            matriz[indexMapLinha-1][indexMapColuna-1] = arrayMap[contMap]
+            
+            if(indexMapColuna === contColunas) {
+                indexMapLinha = 0;
+                indexMapColuna = auxColuna;
+                contLinhas--;
+            }
+        }
+        contMap++;
+    }
+    exibe(matriz)
 });
 
 /*

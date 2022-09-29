7e2f39a77173bfd172544e10b9ee82ca1b3fe41e
Leonardo do Prado <leonardodoprado231@gmail.com>
2022-09-29 14:05:47 -0300

    Att 29-09

--- MPSoC-YX.js
+++ MPSoC-YX.js
@@ -2,7 +2,7 @@ let testData = require('./Teste-TrabalhoMapeamento/TrabalhoMapeamento/Test1.json
 
 let XColunas = parseInt(testData.MPSOC_SIZE_X);
 let YLinhas = parseInt(testData.MPSOC_SIZE_Y);
-let Processor = testData.TASKS_PER_PROCESSOR;
+let TasksProcessor = parseInt(testData.TASKS_PER_PROCESSOR);
 
 console.log(testData)
 
@@ -10,13 +10,15 @@ let contMain = 0;
 
 // Acessando posições das tasks para mapear
 testData.TEST.forEach(TEST => {
-    console.log("\n----------------------------------------------------------------------------------\n")
+    console.log("\n_________________________________________________________________________________________________________________________________\n")
     
+    console.log("-------------------------------------{ TASKS }-------------------------------------\n")
+
     let arrMap = []
 
     console.log(TEST)
-    console.log(TEST.APP) 
-    console.log(TEST.QTD) 
+    //console.log(TEST.APP) 
+    //console.log(TEST.QTD) 
     console.log("\n")
     
     let Application = require(`./Teste-TrabalhoMapeamento/TrabalhoMapeamento/Applications/${TEST.APP}`)
@@ -32,19 +34,22 @@ testData.TEST.forEach(TEST => {
         contTarefas++;
     }
 
-
+    // Multiplicando n (QTD) de vezes as tasks para mapear
     var arr = []
     let contArr = 0
     for (let i = 0; i < TEST.QTD; i++){
         arr.push(arrayMap)
     }
 
+    //console.log(arr)
+    //console.log(arr.length)
+
+    // Unindo os arrays de tasks
     let a = []
     for (let i = 0; i < TEST.QTD; i++){
         a.push(...arr[contArr])
         contArr++;
     }
-
     console.log(a)
 
     //////////////////////////
@@ -53,6 +58,30 @@ testData.TEST.forEach(TEST => {
     for (var i = 0; i < YLinhas; i++) {
         matriz[i] = new Array(XColunas).fill(0);
     }
+
+    //////////////////////////
+    console.log(a.length)
+
+    let contArray = 0;
+    let AuxProcessor = []
+    let ProcessorTask = []
+
+    //console.log(a.length)
+    while(a.length >= contArray){
+        if(AuxProcessor.length < TasksProcessor){
+            AuxProcessor.push(a[contArray])
+        } else {
+            ProcessorTask.push(AuxProcessor)
+            AuxProcessor = []
+            continue;
+        }
+        contArray++;
+    }
+
+    console.log(TasksProcessor)
+    console.log(AuxProcessor.length)
+    console.log(ProcessorTask)
+    /////////////////////////
 
     // Algoritmo de mapeamento
     let indexMapLinha = 0
@@ -63,9 +92,9 @@ testData.TEST.forEach(TEST => {
     let auxColuna = 0;
     let contMap = 0;
 
-    while (a.length > contMap){
+    while (ProcessorTask.length > contMap){
         if (indexMapLinha < contLinhas) {
-            matriz[indexMapLinha][indexMapColuna] = a[contMap]
+            matriz[indexMapLinha][indexMapColuna] = "["+ProcessorTask[contMap]+"]"
             indexMapLinha++;
 
             if(indexMapLinha === contLinhas) {
@@ -74,7 +103,7 @@ testData.TEST.forEach(TEST => {
             }
         } else if (indexMapColuna < contColunas) {
             indexMapColuna++
-            matriz[indexMapLinha-1][indexMapColuna-1] = a[contMap]
+            matriz[indexMapLinha-1][indexMapColuna-1] = "["+ProcessorTask[contMap]+"]"
             
             if(indexMapColuna === contColunas) {
                 indexMapLinha = 0;
@@ -84,7 +113,10 @@ testData.TEST.forEach(TEST => {
         }
         contMap++;
     }
+    console.log("-------------------------------------{ MAP }-------------------------------------\n")
     exibe(matriz)
+
+    console.log(matriz[0][0])
 });
 
 function exibe(matriz) {
